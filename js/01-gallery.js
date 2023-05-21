1bb8cd0950f757a7ae7cd5c925b523ab42ca9e38
Amaryann <121064694+Amaryann@users.noreply.github.com>
2023-05-21 13:06:00 +0300

    /

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+let listLayout = "";
+for (let i = 0; i < galleryItems.length; i++) {
+    listLayout += `<li><a class="gallery__link" href="large-image.jpg"><img class="gallery__image" src="${galleryItems[i].preview}" data-source="${galleryItems[i].original}" alt="${galleryItems[i].description}"></li>`;
+}
+document.querySelector(".gallery").insertAdjacentHTML("afterbegin", listLayout)
+document.querySelectorAll(".gallery__link").forEach(element => {
+    element.addEventListener("click", (e)=>{
+        e.preventDefault();
+        const html = `<img width="1400" height="900" src="${e.target.getAttribute("data-source")}">`
+        basicLightbox.create(html).show()
+    })
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+let listLayout = "";
+for (let i = 0; i < galleryItems.length; i++) {
+    listLayout += `<li><a class="gallery__link" href="${galleryItems[i].original}"><img class="gallery__image" src="${galleryItems[i].preview}" alt="${galleryItems[i].description}"></li>`;
+}
+document.querySelector(".gallery").insertAdjacentHTML('afterbegin', listLayout)
+document.querySelectorAll(".gallery__link").forEach(element => {
+   element.addEventListener("click", (e)=>{
+    e.preventDefault();
+    let lightbox = new SimpleLightbox(element, {captionsData: "alt"})
+    lightbox.open()
+   }) 
+}); 
